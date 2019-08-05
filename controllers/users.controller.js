f55b42177b030f7e0d6f2bf8bc4d2204ee551f87
DevGeek <iamdevgeek@gmail.com>
2019-08-05 13:49:50 +0530

    Few minor changes

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -5,15 +5,19 @@ var jwt = require('jsonwebtoken');
 
 exports.create = function (req, res, next) {
     var filestatus = '';
-    if (req.files &&  Object.keys(req.files).length > 0) {
+
+    if (!req.body.username || !req.body.password) {
+        res.status(200).json({ 'message': 'Username and password both are required' });
+    }
+    if (req.files && Object.keys(req.files).length > 0) {
         // return res.status(400).send('No files were uploaded.');
         // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
         let sampleFile = req.files.profilepicture;
         req.body.profilepicture = sampleFile.name;
-        sampleFile.mv('./public/userphotos/'+sampleFile.name, function(err) {
+        sampleFile.mv('./public/userphotos/' + sampleFile.name, function (err) {
             if (err)
                 return res.status(500).send(err);
-            
+
             filestatus = ' - File uploaded!';
         });
     }
@@ -24,18 +28,18 @@ exports.create = function (req, res, next) {
         // var token = jwt.sign({ id: user._id }, config.secret, {
         //     expiresIn: 86400 // expires in 24 hours
         // });
-        res.status(200).json({ 'message': 'User created Successfully'+filestatus })
+        res.status(200).json({ 'message': 'User created Successfully' + filestatus })
     })
         .catch(err => {
             res.status(400).send({ 'Error while creating user.': err });
         });
 };
 exports.createForm = function (req, res, next) {
-    res.render('createForm', {title: 'Create a user'});
+    res.render('createForm', { title: 'Create a user' });
 };
 
 exports.update = function (req, res, next) {
-    if(req.body.password) {
+    if (req.body.password) {
         req.body.password = crypt().encrypt(req.body.password);
     }
     usersSchema.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, user) => {
@@ -59,14 +63,14 @@ exports.delete = function (req, res, next) {
 
 exports.list = function (req, res, next) {
     var perPage = parseInt(req.params.limit);
-    var page = Math.max(0, req.params.page); 
+    var page = Math.max(0, req.params.page);
     usersSchema.find((err, users) => {
         if (err) {
             return res.status(400).send({ message: 'Unable to fetch ussers!!!', err: err });
         } else {
             res.status(200).send(users);
         }
-    }).skip(perPage*page).limit(perPage).sort({
+    }).skip(perPage * page).limit(perPage).sort({
         username: 'asc'
     });
 };
@@ -79,5 +83,5 @@ exports.search = function (req, res, next) {
             res.status(200).send(users);
         }
     }).catch(err => console.log('Caught: ', err.message));
-    
+
 };
